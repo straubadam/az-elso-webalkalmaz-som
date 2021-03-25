66a3a23ad6f62a488b0e0b0c2592832d512a1e67
straubadam <straub.adam@gmail.com>
2021-03-25 19:14:52 +0100

    jobbra-balra

--- scripts.js
+++ scripts.js
@@ -5,42 +5,42 @@ let data = [
         description: 'Túrázás közben egy pacival'
     },
 
-        {
+    {
         photo: './images/kep2.jpg',
         title: 'Az új tévém',
         description: 'Végül eladtam, mert túl sötét volt a képe.'
     },
-    
-        {
+
+    {
         photo: './images/kep3.jpg',
         title: 'Fő a biztonság',
         description: 'Ne felejtsük otthon a maszkot a közlekedés közben!'
     },
 
-        {
+    {
         photo: './images/kep4.jpg',
-        title: '',
+        title: 'Haho',
         description: 'Ne felejtsük otthon a maszkot a közlekedés közben!'
     },
 
-        {
+    {
         photo: './images/kep5.jpg',
         title: 'Ez egy kereszt',
         description: 'Üdvözlünk a Nagy Szénás hegyen!'
     },
-    
-        {
+
+    {
         photo: './images/kep6.jpg',
         title: 'Nagy Szénás',
         description: 'Szuper a kilátás, megéri feljönni a csúcsra!'
     },
 
-        {
+    {
         photo: './images/kep7.jpg',
         title: 'Autópálya',
         description: 'Ez direkt ilyen, nem a kép csúszott szét.'
     },
-        {
+    {
         photo: './images/kep8.jpg',
         title: 'Fagy Bokodon',
         description: 'A Bokodi tó népszerű tó az Instagram felhasználóinak népszerű helyszíne.'
@@ -51,9 +51,28 @@ let data = [
 let currentPhoto = 0;
 $('#photo').attr('src', data[currentPhoto].photo);
 
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', data[currentPhoto].photo);
+}
+
+$('#jobbranyil').click(() => {
+    if (currentPhoto > 0) {
+    currentPhoto++}
+    loadPhoto(currentPhoto);
+  })
+
+  $('#balranyil').click(() => {
+    if (currentPhoto < 9) { 
+        current++; }
+    currentPhoto--;
+    loadPhoto(currentPhoto);
+  })
 
 
 /*
+
+
+
 Ez Csaba kódja elemzéshez:
 let loadPhoto = (photoNumber) => {
     $('#photo').attr('src', data[photoNumber].photo);
@@ -66,7 +85,7 @@ $('#balranyil').click (() => {
     } else {
         currentPhoto = 9;
         loadPhoto(currentPhoto);
-    }   
+    }
 })
 
 $('#jobbranyil').click (() => {
@@ -76,7 +95,7 @@ $('#jobbranyil').click (() => {
     } else {
         currentPhoto ++;
         loadPhoto(currentPhoto);
-    }   
+    }
 })
 
 */
